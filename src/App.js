607c0adb76978d3d777212cc3f3ea8887b8882a5
Pavel Che <alien.killer3@bk.ru>
2021-10-27 17:24:11 +0300

    built hierarchy of folders and install 'node-sass'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-        hello world
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        
+      </div>
+    )
+  }
 }
 
-export default App;
