76ca4878a253a760c4c30978991d51c78423c6c6
Pavel Che <alien.killer3@bk.ru>
2021-11-01 12:04:26 +0300

    connected formik

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -29,17 +29,18 @@ const Message = ({ avatar, user, text, date, isMe, isReaded, attachments }) => (
                 <div className="message__bubble">
                     <p className="message__text">{text}</p>
                 </div>
+                <div className="message__attachments">
+                    {
+                        attachments && attachments.map(item => (
+                            <div className="message__attachments-item">
+                                <img src={item.url} alt={item.filename} />
+                            </div>
+                        ))
+                    }
+                </div>
                 <span className="message__date">{formatDistanceToNow(date, { addSuffix: true, locale: ruLocale })}</span>
             </div>
-            <div className="message__attachments">
-                {
-                    attachments && attachments.map(item => (
-                        <div className="message__attachments-item">
-                            <img src={item.url} alt={item.filename} />
-                        </div>
-                    ))
-                }
-            </div>
+
         </div>
     </div>
 )
--- src/modules/RegisterForm/components/RegisterForm.jsx
+++ src/modules/RegisterForm/components/RegisterForm.jsx
@@ -4,61 +4,69 @@ import { Form, Input } from 'antd';
 import { UserOutlined, LockOutlined, MailOutlined, InfoCircleTwoTone } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
-class RegisterForm extends Component {
-    render() {
-        const success = false;
-        return (
-            <div>
-                <div className="auth__top">
-                    <h2>Регистрация</h2>
-                    <p>Для входа в чат, вам нужно зарегистрироваться</p>
-                </div>
-                <Block>
-                    {!success ? (
-                        <Form className="login-form">
-                            <Form.Item validateStatus="success" name="username" hasFeedback>
-                                <Input prefix={<UserOutlined className="site-form-item-icon" style={{color: '#ccc'}}/>} placeholder="Ваше имя" size="large" />
-                            </Form.Item>
-                            <Form.Item validateStatus="success" name="username" hasFeedback>
-                                <Input prefix={<MailOutlined style={{ color: '#ccc' }}/>} placeholder="E-mail" size="large" />
-                            </Form.Item>
-                            <Form.Item name="password">
-                                <Input
-                                    prefix={<LockOutlined className="site-form-item-icon" style={{ color: '#ccc' }}/>}
-                                    type="password"
-                                    placeholder="Пароль"
-                                    size="large"
-                                />
-                            </Form.Item>
-                            <Form.Item name="password">
-                                <Input
-                                    prefix={<LockOutlined className="site-form-item-icon" style={{ color: '#ccc' }}/>}
-                                    type="password"
-                                    placeholder="Повторите пароль"
-                                    size="large"
-                                />
-                            </Form.Item>
+const success = false;
 
-                            <Form.Item>
-                                <Button type="primary" htmlType="submit" size="large">
-                                    Зарегистрироваться
-                                </Button>
-                            </Form.Item>
-                            <Link className="auth__register-link" to="/login">Войти в аккаунт</Link>
-                        </Form>
-                    ) : (
-                        <div className="auth__success-block">
-                            <div>
-                                    <InfoCircleTwoTone style={{ fontSize: '48px'}}/>
-                            </div>
-                            <h2>Потвердите свой аккаунт</h2>
-                            <p>На вашу почту отправлено письмо с ссылкой на потверждение аккаунта</p>
-                        </div>
-                    )}
-                </Block>
+const RegisterForm = props => {
+    const {
+        values,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+    } = props;
+    
+    return (
+        <div>
+            <div className="auth__top">
+                <h2>Регистрация</h2>
+                <p>Для входа в чат, вам нужно зарегистрироваться</p>
             </div>
-        )
-    }
+            <Block>
+                {!success ? (
+                    <Form onSubmit={handleSubmit} className="login-form">
+                        <Form.Item validateStatus="success" name="username" hasFeedback>
+                            <Input prefix={<UserOutlined className="site-form-item-icon" style={{ color: '#ccc' }} />} placeholder="Ваше имя" size="large" />
+                        </Form.Item>
+                        <Form.Item validateStatus="success" name="email" hasFeedback>
+                            <Input prefix={<MailOutlined style={{ color: '#ccc' }} />} placeholder="E-mail" size="large" />
+                        </Form.Item>
+                        <Form.Item name="password">
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" style={{ color: '#ccc' }} />}
+                                type="password"
+                                placeholder="Пароль"
+                                size="large"
+                            />
+                        </Form.Item>
+                        <Form.Item name="password">
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" style={{ color: '#ccc' }} />}
+                                type="password"
+                                placeholder="Повторите пароль"
+                                size="large"
+                            />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" size="large">
+                                Зарегистрироваться
+                            </Button>
+                        </Form.Item>
+                        <Link className="auth__register-link" to="/login">Войти в аккаунт</Link>
+                    </Form>
+                ) : (
+                    <div className="auth__success-block">
+                        <div>
+                            <InfoCircleTwoTone style={{ fontSize: '48px' }} />
+                        </div>
+                        <h2>Потвердите свой аккаунт</h2>
+                        <p>На вашу почту отправлено письмо с ссылкой на потверждение аккаунта</p>
+                    </div>
+                )}
+            </Block>
+        </div>
+    )
 }
 
 export default RegisterForm
