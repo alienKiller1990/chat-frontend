dd8b38876914b7003e43561f6306ebdca60b7661
Pavel Che <alien.killer3@bk.ru>
2021-11-10 19:39:02 +0300

    changed sample of colors for DialogItem component

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { generateAvatarFromHash } from 'utils/helpers'
+// import { generateAvatarFromHash } from 'utils/helpers'
+import randomColor from 'randomcolor';
+import tinycolor from 'tinycolor2';
+
 
 
 
@@ -11,10 +14,15 @@ const Avatar = ({ user }) => {
             <img className="avatar" src={user.avatar} alt={`Avatar ${user.fullname}`} />
         );
     } else {
-        const {color, colorLighten} = generateAvatarFromHash(user._id);
+        // const {color, colorLighten} = generateAvatarFromHash(user._id);
+        const color = randomColor({
+            luminosity: 'dark',
+        });
+        const colorLight = tinycolor(color).lighten(30)
+        console.log(color)
         const firstChar = user.fullname[0]
         return <div
-            style={{ background: `linear-gradient(135deg, ${color} 0%, ${colorLighten} 96%)`}}
+            style={{ background: `linear-gradient(135deg, ${color} 0%, ${colorLight} 96%)`}}
             className="avatar avatar--symbol">{firstChar.toUpperCase()}</div>
     }
 }
--- src/utils/helpers/generateAvatarFromHash.js
+++ src/utils/helpers/generateAvatarFromHash.js
@@ -1,23 +1,31 @@
+import randomColor from 'randomcolor';
 import tinycolor from 'tinycolor2';
 
 
-const getCorrectIndex = number => {
-    return number > 255 ? 255 : number < 0 ? 0 : number;
+// const getCorrectIndex = number => {
+//     return number > 255 ? 255 : number < 0 ? 0 : number;
+// }
+
+const getSumofNumber = str => {
+    return [...str].reduce((accum, item) => accum + item.charCodeAt(),0)
 }
+
+
 
 
 export default (hash) => {
     const [r, g, b] = hash
-        .substr(0, 3)
-        .split('')
-        .map(char => getCorrectIndex(char.charCodeAt(0)));
+        .substr(0, 6)
+        .match(/../g)
+        .map(item => getSumofNumber(item));
+        console.log(r, g, b)
     return {
         color:
             tinycolor({ r, g, b })
                 .toHexString(),
         colorLighten:
             tinycolor({ r, g, b })
-                .lighten(50)
+                .lighten(30)
                 .toHexString()
 
     };
